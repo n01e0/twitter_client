a9777b727f172d75cd9ff5b3bad24226778e47fc
n01e0 <noleo@vanillabeans.mobi>
2020-01-31 17:21:36 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,35 @@
-extern crate oauthcli;
-extern crate ureq;
-extern crate url;
-extern crate percent_encoding;
-extern crate serde;
-extern crate serde_json;
-#[macro_use]
-extern crate serde_derive;
-extern crate toml;
-
-use std::fs;
-use std::env;
-use serde_json::Result;
-use oauthcli::*;
-use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
-
-const FLAG: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`').add(b'+').add(b'!').add(b'?').add(b'"').add(b'\'').add(b'$').add(b'@').add(b'#').add(b'%').add(b'^').add(b'&').add(b'*').add(b'(').add(b')').add(b'=').add(b'+').add(b'[').add(b']').add(b'{').add(b'}').add(b';').add(b':').add(b'/').add(b',').add(b'`');
-
-#[derive(Debug, Deserialize)]
-struct Config {
-    api_key: String,
-    api_secret: String,
-    token: String,
-    token_secret: String 
-}
-
-#[derive(Deserialize, Debug)] 
-struct Errors {
-    code: usize,
-    message: String
-}
-#[derive(Deserialize, Debug)] 
-struct Response {
-    errors: Vec<Errors>
-}
+mod twitter;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config_path = format!("{}/.twitter_cli/config.toml", env::var("HOME").unwrap());
-    match fs::read_to_string(&config_path) {
-        Ok(content) =>  {
-            let conf: Config = toml::from_str(&content).unwrap();
-            if args.len() > 1 {
-                tweet(&args[1], conf);
+    let args: Vec<String> = std::env::args().collect();
+    match args.len() {
+        1 => {
+            let resp = twitter::TwitterBuilder::new().get().finish().call();
+            if let Some(err_resp) = resp.error {
+                for error in err_resp.errors {
+                    eprintln!("{}: {}", &error.code, &error.message);
+                }
             } else {
-                timeline(conf);
+                let tw = resp.tweet.unwrap().tweet_data;
+                println!("{}: {}", &tw.user.name, &tw.text);
             }
         },
-        Err(err) => eprintln!("{}", err),
+
+        2 => {
+            let resp = twitter::TwitterBuilder::new().post(args[1].clone()).finish().call();
+            if let Some(err_resp) = resp.error {
+                for error in err_resp.errors {
+                    eprintln!("{}: {}", &error.code, &error.message);
+                }
+            } else {
+                println!("Tweeted!: \"{}\"", args[1]);
+            }
+        },
+        
+        _ => usage(args[0].clone()),
     }
 }
 
-fn tweet(content: &str, config: Config) {
-    let formated_content = format!("status={}", utf8_percent_encode(content, FLAG).to_string());
-    let url = url::Url::parse(&format!("https://api.twitter.com/1.1/statuses/update.json?{}", formated_content)).unwrap();
-    let header =
-        OAuthAuthorizationHeaderBuilder::new(
-            "POST", &url, &config.api_key, &config.api_secret, SignatureMethod::HmacSha1)
-        .token(&config.token, &config.token_secret)
-        .finish_for_twitter();
-    
-    let resp = ureq::post(url.as_str())
-                .set("Authorization", &format!("OAuth {}", header.auth_param()))
-                .call();
-    if resp.ok() {
-        println!("Tweeted! \"{}\"", content);
-    } else {
-        let err: Result<Response> = serde_json::from_str(&resp.into_string().unwrap());
-        match err {
-            Ok(response) => {
-                eprintln!("Error code:{}\n\t{}", &response.errors[0].code, &response.errors[0].message);
-                eprintln!("Send data = {}", formated_content);
-            },
-            Err(err) => eprintln!("{}", err),
-        }
-    }
-}
-
-
-fn timeline(config: Config) {
-    #[derive(Deserialize, Debug)] 
-    struct User {
-        name: String
-    }
-    
-    #[derive(Deserialize, Debug)] 
-    struct TweetData {
-        text: String,
-        user: User
-    }
-    #[derive(Deserialize, Debug)] 
-    struct Json {
-        tw: TweetData
-    }
-
-    let url = url::Url::parse("https://api.twitter.com/1.1/statuses/home_timeline.json?count=1").unwrap();
-    let header =
-        OAuthAuthorizationHeaderBuilder::new(
-            "GET", &url, config.api_key, config.api_secret, SignatureMethod::HmacSha1)
-        .token(&config.token, &config.token_secret)
-        .finish_for_twitter();
-    
-    let resp = ureq::get(url.as_str())
-                .set("Authorization", &format!("OAuth {}", header.auth_param()))
-                .call();
-    if resp.ok() {
-        let top: Result<Json> = serde_json::from_str(&resp.into_string().unwrap());
-        match top {
-            Ok(top) => println!("{}: {}", &top.tw.user.name, &top.tw.text),
-            Err(err) => eprintln!("Error: {}", err),
-        }
-    } else {
-        eprintln!("{}: {}", &resp.status(), &resp.status_text());
-    }
+fn usage(name: String) {
+    println!("Usage: {} <content>", name);
 }
---
+++ src/twitter.rs
@@ -0,0 +1,225 @@
+extern crate oauthcli;
+extern crate ureq;
+extern crate url;
+extern crate percent_encoding;
+extern crate serde;
+extern crate serde_json;
+extern crate toml;
+
+use std::fs;
+use std::io::{Write, stdin};
+use std::env;
+use serde::{Deserialize, Serialize};
+use oauthcli::*;
+use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
+
+const FLAG: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`').add(b'+').add(b'!').add(b'?').add(b'"').add(b'\'').add(b'$').add(b'@').add(b'#').add(b'%').add(b'^').add(b'&').add(b'*').add(b'(').add(b')').add(b'=').add(b'+').add(b'[').add(b']').add(b'{').add(b'}').add(b';').add(b':').add(b'/').add(b',').add(b'`');
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Config {
+    api_key:        String,
+    api_secret:     String,
+    token:          String,
+    token_secret:   String 
+}
+
+#[derive(Deserialize, Debug, Clone)] 
+pub struct Errors {
+    pub code:       usize,
+    pub message:    String
+}
+
+#[derive(Deserialize, Debug, Clone)] 
+pub struct ErrorResponse {
+    pub errors: Vec<Errors>
+}
+
+#[derive(Deserialize, Debug, Clone)] 
+pub struct User {
+    pub name: String
+}
+
+#[derive(Deserialize, Debug, Clone)] 
+pub struct TweetData {
+    pub text: String,
+    pub user: User
+}
+#[derive(Deserialize, Debug, Clone)] 
+pub struct TweetResponse {
+    pub tweet_data: TweetData
+}
+
+#[derive(Clone, Copy)]
+pub enum Method {
+    Get,
+    Post
+}
+
+impl Method {
+    pub fn to_str(&self) -> &str {
+        match self {
+            Method::Get => "GET",
+            Method::Post => "POST",
+        }
+    }
+}
+
+pub struct Twitter {
+    method:     Method,
+    url:        url::Url,
+    header:     oauthcli::OAuthAuthorizationHeader
+}
+
+pub struct TwitterBuilder {
+    config:         Config,
+    method:         Method,
+    tweet_content:  Option<String>
+}
+
+pub fn configure(config_dir: String, filename: String) {
+    println!("fopen");
+    let config_file = fs::File::create(&format!("{}/{}", &config_dir, &filename));
+    let mut content = Config {
+        api_key: String::new(),
+        api_secret: String::new(),
+        token: String::new(),
+        token_secret: String::new()
+    };
+    match config_file {
+        Ok(mut file) =>  {
+            println!("Found config file");
+            println!("Let's configure");
+            let mut buf = String::new();
+            println!("Input your API_KEY");
+            stdin().read_line(&mut buf).unwrap();
+            content.api_key = buf.trim().parse().ok().unwrap();
+            buf.clear();
+            println!("api_secret");
+            stdin().read_line(&mut buf).unwrap();
+            content.api_secret = buf.trim().parse().ok().unwrap(); 
+            buf.clear();
+            println!("token");
+            stdin().read_line(&mut buf).unwrap();
+            content.token = buf.trim().parse().ok().unwrap();
+            buf.clear();
+            println!("token_secret");
+            stdin().read_line(&mut buf).unwrap();
+            content.token_secret = buf.trim().parse().ok().unwrap();
+            buf.clear();
+            let toml = toml::to_string(&content).unwrap();
+            write!(file, "{}", toml).unwrap();
+        },
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => {
+                println!("Not found");
+                if let Err(err) = std::fs::create_dir(&config_dir) {
+                    eprintln!("{}", err);
+                } else {
+                    configure(config_dir, filename);
+                }
+            },
+            err => eprintln!("{}", std::io::Error::from(err)),
+        }
+    }
+}
+
+impl TwitterBuilder {
+    pub fn new() -> TwitterBuilder {
+        let config_dir = format!("{}/.twitter_client", env::var("HOME").unwrap());
+        let config_filename = "config.toml".to_string();
+        let path = format!("{}/{}", config_dir, config_filename);
+        let config_data = match fs::read_to_string(&path) {
+            Ok(data) => data,
+            Err(_) =>  {
+                configure(config_dir, config_filename);
+                fs::read_to_string(&path).unwrap()
+            },
+        };
+        TwitterBuilder {
+            config: toml::from_str(&config_data).unwrap(),
+            method: Method::Get,
+            tweet_content: None
+        }
+    }
+
+    pub fn get(mut self) -> TwitterBuilder {
+        self.method = Method::Get;
+        self
+    }
+
+    pub fn post(mut self, content: String) -> TwitterBuilder {
+        self.method = Method::Post;
+        self.tweet_content = Some(format!("status={}",utf8_percent_encode(&content, FLAG).to_string()));
+        self
+    }
+
+    pub fn finish(&mut self) -> Twitter {
+        let url = match self.method {
+                Method::Get => {
+                    url::Url::parse("https://api.twitter.com/1.1/statuses/home_timeline.json?count=1").unwrap()
+                },
+                Method::Post => {
+                    url::Url::parse(
+                        &format!("https://api.twitter.com/1.1/statuses/update.json?{}", &self.tweet_content.clone().unwrap()))
+                        .unwrap()
+                }
+        };
+        let header = OAuthAuthorizationHeaderBuilder::new(self.method.to_str(),
+                &url, &self.config.api_key, &self.config.api_secret, SignatureMethod::HmacSha1)
+                .token(&self.config.token, &self.config.token_secret)
+                .finish_for_twitter();
+        
+        Twitter {
+            url: url,
+            method: self.method,
+            header: header
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Response {
+    pub tweet: Option<TweetResponse>,
+    pub error: Option<ErrorResponse>
+}
+
+impl Twitter {
+    pub fn call(self) -> Response {
+        match self.method {
+            Method::Get =>  {
+                let resp = ureq::get(self.url.as_str())
+                    .set("Authorization", &format!("OAuth {}", self.header.auth_param()))
+                    .call();
+                if resp.ok() {
+                    return Response {
+                        tweet: Some(serde_json::from_str(&resp.into_string().unwrap()).unwrap()),
+                        error: None
+                    }
+                } else {
+                    return Response {
+                        tweet: None,
+                        error: Some(serde_json::from_str(&resp.into_string().unwrap()).unwrap())
+                    }
+                }
+            },
+
+            Method::Post => {
+                let resp = ureq::post(self.url.as_str())
+                    .set("Authorization", &format!("OAuth {}", self.header.auth_param()))
+                    .call();
+                if resp.ok() {
+                    return Response {
+                        tweet: None,
+                        error: None
+                    }
+                } else {
+                    return Response {
+                        tweet: None,
+                        error: Some(serde_json::from_str(&resp.into_string().unwrap()).unwrap())
+                    }
+                }
+            }
+        }
+    }
+}
+
